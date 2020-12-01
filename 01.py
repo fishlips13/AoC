db49faa01c5918c9afdb2b10c1cedd9e5d724678
fishlips13 <fishlips13@gmail.com>
2020-12-01 17:57:22 +0000

    Update 01.py

--- 01.py
+++ 01.py
@@ -9,6 +9,7 @@ def willItBlend2020(data, count):
     for comb in combinations(data, count):
         if sum(comb) == 2020:
             return "{} = {}, {}".format(" + ".join(map(str, comb)), str(sum(comb)), str(prod(comb)))
+    return "Nope"
 
 print("Part 1: " + willItBlend2020(data, 2))
 print("Part 2: " + willItBlend2020(data, 3))
