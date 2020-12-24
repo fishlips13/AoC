e13437bbb6115f1ddd5d1c1db52208a0564d483d
fishlips13 <fishlips13@gmail.com>
2020-12-24 04:59:25 +0000

    Day 14

--- 13.py
+++ 13.py
@@ -1,6 +1,6 @@
 with open("input/13.txt") as f:
     earliest_time = int(f.readline())
-    schedule = [int(i) if i.isnumeric() else i for i in f.readline().split(",")]
+    schedule = [int(i) if i.isnumeric() else 0 for i in f.readline().split(",")]
 
 earliest_bus_id = 0
 earliest_bus_time = 999999999999999999
@@ -8,7 +8,7 @@ earliest_bus_time = 999999999999999999
 product = 1
 
 for bus in schedule:
-    if bus == "x":
+    if not bus:
         continue
 
     product *= bus
@@ -21,14 +21,11 @@ for bus in schedule:
 print(f"Earliest Bus Wait x ID: {earliest_bus_id * (earliest_bus_time - earliest_time)}")
 
 x = 0
-for i in range(len(schedule)):
-    bus = schedule[i]
-    if bus == "x":
+for i, bus in enumerate(schedule):
+    if not bus:
         continue
 
-    n = (product // bus)
+    n = product // bus
     x += -i * n * pow(n, -1, bus)
 
-answer = x % product
-
-print(f"Competition Answer: {answer}")
+print(f"Competition Answer: {x % product}")
